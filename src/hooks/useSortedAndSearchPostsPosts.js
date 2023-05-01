3559646b32bd635706558b962a74508b11573df1
modiconme <rebeelsc@gmail.com>
2023-05-01 14:44:39 +0700

    init

---
+++ src/hooks/useSortedAndSearchPostsPosts.js
@@ -0,0 +1,11 @@
+import {computed, ref} from "vue";
+
+export default function useSortedAndSearchPostsPosts(sortedPosts) {
+    const searchQuery = ref('')
+    const sortedAndSearchPosts = computed(() => {
+        return sortedPosts.value.filter(post => post.title.toLowerCase().includes(searchQuery.value.toLowerCase()))
+    })
+    return {
+        searchQuery, sortedAndSearchPosts
+    }
+}
